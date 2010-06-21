04cd64e52e78849e0d50fc8f305030436fe486bb
Christian Stocker <me@chregu.tv>
2010-06-21 09:30:46 +0200

    new intervals

--- playnice.php
+++ playnice.php
@@ -14,8 +14,8 @@
 // MIT license.
 //
 
-define("MIN_INTERVAL", 9); // minimal interval in minutes
-define("MAX_INTERVAL", 29); // maximum interval in minutes
+define("MIN_INTERVAL", 120); // minimal interval in minutes
+define("MAX_INTERVAL", 180); // maximum interval in minutes
 define("POLLS_BEFORE_MAX", 5); // take this many polls to reach the max polling interval
 
 define("BASE_PATH", dirname(__FILE__));
